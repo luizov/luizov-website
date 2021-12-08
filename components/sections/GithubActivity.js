192a796880083fcb52e3ac95a20f0e1f56eb9038
luizov <luizovdimitur@gmail.com>
2021-12-08 22:03:42 +0200

    :lipstick: Subtle design changes

--- components/sections/GithubActivity.js
+++ components/sections/GithubActivity.js
@@ -3,16 +3,18 @@ import Image from 'next/image';
 
 const RepoList = ({ repos, title, url }) => {
     return (
-        <div>
-            <h3 className="mt-1 mb-8 text-2xl tracking-tight text-mauve-11 dark:text-mauveDark-11">
+        <div className="mt-12">
+            <h3 className="mt-1 text-2xl tracking-tight text-mauve-11 dark:text-mauveDark-11">
                 {title}
             </h3>
-            <ul>
+            <ul className="grid grid-cols-1 gap-y-6 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-6 mb-8">
                 {repos.map(({ id, name, url, description, owner }) => (
                     <li key={id}>
                         <a
                             href={url}
-                            className="block my-4 p-4 sm:p-6 sm:border border-mauve-7 dark:border-mauveDark-7 hover:bg-mauve-4 dark:hover:bg-mauveDark-4 rounded-xl transition duration-200"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="block my-4 p-4 sm:p-6 h-full sm:border border-mauve-7 dark:border-mauveDark-7 hover:bg-mauve-4 dark:hover:bg-mauveDark-4 rounded-xl transition duration-200"
                         >
                             <div className="flex items-center mb-3">
                                 <div className="w-10 h-10 mr-3 relative">
@@ -27,11 +29,12 @@ const RepoList = ({ repos, title, url }) => {
                                 <span className="text-mauve-12 dark:text-violetDark-11">
                                     {owner.login}/
                                 </span>
-                                <span className="text-blue-500 dark:text-mauveDark-12">
+                                <span className="text-blue-500 dark:text-mauveDark-12 truncate">
                                     {name}
                                 </span>
                             </div>
-                            <div className="text-mauve-11 dark:text-mauveDark-11">      {description}
+                            <div className="text-mauve-11 dark:text-mauveDark-11">
+                                {description}
                             </div>
                         </a>
                     </li>
@@ -61,18 +64,18 @@ export const GithubActivity = ({ starredRepos, contributedRepos }) => {
                 <h2 className="text-4xl text-mauve-12 dark:text-mauveDark-12 font-semibold tracking-tight">
                     GitHub Activity
                 </h2>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-6">
-                    <RepoList
-                        title="Repos I Contributed to"
-                        url={`https://github.com/${config.githubUsername}`}
-                        repos={contributedRepos}
-                    />
-                    <RepoList
-                        title="Repos I Like"
-                        repos={starredRepos}
-                        url={`https://github.com/${config.githubUsername}?tab=stars`}
-                    />
-                </div>
+
+                <RepoList
+                    title="Repos I Contributed to"
+                    url={`https://github.com/${config.githubUsername}`}
+                    repos={contributedRepos}
+                />
+                <RepoList
+                    title="Repos I Like"
+                    repos={starredRepos}
+                    url={`https://github.com/${config.githubUsername}?tab=stars`}
+                />
+
             </div>
         </section>
     )
