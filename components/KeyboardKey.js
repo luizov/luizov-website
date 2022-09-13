385423cd246f40ad7091d66c82483ad1c07a3d77
luizov <luizovdimitur@gmail.com>
2022-09-13 17:21:12 +0300

    bug: fix display of keyboard key component

--- components/KeyboardKey.js
+++ components/KeyboardKey.js
@@ -1,9 +1,9 @@
 export default function KeyboardKey({ children }) {
 	let key = children || '';
-	key = key.length > 0 ? key.toLowerCase() : key.toUpperCase();
+	key = key.length > 1 ? key.toLowerCase() : key.toUpperCase();
 
 	return (
-		<kbd className="w-7 h-7 inline-flex items-center justify-center select-none text-base font-mono font-medium rounded text-slate-11 bg-slate-2 dark:text-whiteA-11 dark:bg-skyDark-5 shadow">
+		<kbd className="w-7 h-7 inline-flex items-center justify-center select-none text-sm font-mono font-medium rounded text-slate-11 bg-slate-2 dark:text-whiteA-11 dark:bg-skyDark-5 shadow">
 			{key}
 		</kbd>
 	)
