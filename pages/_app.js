8c89a68d76ef4c1f3a429b54ac4de0c9cc2fdd84
luizov <luizovdimitur@gmail.com>
2023-03-15 10:17:02 +0200

    add Beam Analytics script for tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/fonts.scss';
 import '@/styles/main.scss';
+import Script from 'next/script';
 import { useRouter } from 'next/router';
 import { ThemeProvider } from 'next-themes';
 import {
@@ -15,25 +16,30 @@ function MyApp({ Component, pageProps }) {
 	const Layout = Component.layout || (({ children }) => children);
 
 	return (
-		<ThemeProvider
-			attribute="class"
-			disableTransitionOnChange
-		>
-			<SEO />
-			<CommandBar>
-				<LazyMotion features={domAnimation}>
-					<AnimatePresence
-						mode="wait"
-						initial={false}
-						onExitComplete={() => window.scrollTo(0, 0)}
-					>
-						<Layout key={router.route}>
-							<Component {...pageProps} />
-						</Layout>
-					</AnimatePresence>
-				</LazyMotion>
-			</CommandBar>
-		</ThemeProvider>
+		<>
+			<Script
+				src="https://beamanalytics.b-cdn.net/beam.min.js" data-token="066179c3-ac1f-458b-8282-4430a803ba86"
+			/>
+			<ThemeProvider
+				attribute="class"
+				disableTransitionOnChange
+			>
+				<SEO />
+				<CommandBar>
+					<LazyMotion features={domAnimation}>
+						<AnimatePresence
+							mode="wait"
+							initial={false}
+							onExitComplete={() => window.scrollTo(0, 0)}
+						>
+							<Layout key={router.route}>
+								<Component {...pageProps} />
+							</Layout>
+						</AnimatePresence>
+					</LazyMotion>
+				</CommandBar>
+			</ThemeProvider>
+		</>
 	)
 }
 
